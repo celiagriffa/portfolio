a22d686b703f69f48972da2bc66940c0a12eb037
erKripad <dcrip03@gmail.com>
2026-06-04 10:21:45 +0200

    sitemap - added

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/portfolio/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({
+      hostname: 'https://https://celiagriffa.github.io',
+    })
+  ]
 })
